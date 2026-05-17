f1a78234f7b4fd85b31d2165100ad7c3d36be4bd
phunyezwa <poomeigh503@gmail.com>
2026-05-17 09:37:35 +0200

    fix: restore CSS import for styling

--- src/client.tsx
+++ src/client.tsx
@@ -2,6 +2,7 @@ import { hydrateRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
+import "./styles.css";
 
 const queryClient = new QueryClient();
 
